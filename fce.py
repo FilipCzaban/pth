6382f8376294b7c6d6c6e171cf178ad1a4862f65
FilipCzaban <betkavodakova@seznam.cz>
2025-01-12 01:56:28 +0100

    Add files via upload

---
+++ fce.py
@@ -0,0 +1,18 @@
+def absl(n):
+    if n < 0:
+        n = n * -1
+    return n
+x = (absl(-2)+1)
+cl = 5
+if x < cl: 
+    while x < cl:
+        x += 1
+    print(x)
+else: print (x)       
+   
+ 
+   
+    
+
+
+
