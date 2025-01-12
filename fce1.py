11cc2ceff40bf07d048dba4e6cf0aa2ed7adbf07
FilipCzaban <betkavodakova@seznam.cz>
2025-01-12 01:58:36 +0100

    Add files via upload

---
+++ fce1.py
@@ -0,0 +1,15 @@
+def fce(písm, pct):
+    for i in range(pct):
+        print(písm, end="")
+        print()
+        return pct
+výsledek = fce(100,1)
+def secist(a, b):
+    return a + b
+kncvysledek = secist(výsledek,10)
+print("Součet:", kncvysledek)    
+   
+    
+
+
+
